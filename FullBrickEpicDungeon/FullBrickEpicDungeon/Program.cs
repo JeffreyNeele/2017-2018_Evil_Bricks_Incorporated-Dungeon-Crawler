b7f3fffef87503861da6366be5afe3d1ef0df3a8
patrick <p.h.klaassen@students.uu.nl>
2017-11-15 11:33:14 +0100

    game = new dungeoncrawler ipv. game1

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Program.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Program.cs
@@ -14,7 +14,7 @@ namespace FullBrickEpicDungeon
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new DungeonCrawler())
                 game.Run();
         }
     }
