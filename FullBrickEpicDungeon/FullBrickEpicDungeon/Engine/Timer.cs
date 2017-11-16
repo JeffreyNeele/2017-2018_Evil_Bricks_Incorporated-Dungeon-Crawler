25e66ea9309c5b6e63826299344010f23d510488
Lightning2x <schagen34@gmail.com>
2017-11-16 21:38:26 +0100

    Added a timer class in the Engine

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/Timer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+
+class Timer
+{
+    private float targettime, currentime;
+    private bool paused;
+    public Timer(float targettime)
+    {
+        this.targettime = targettime;
+        currentime = 0;
+        paused = false;
+    }
+
+    private void Update(GameTime gameTime)
+    {
+        if (!paused)
+        {
+            currentime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+
+    public bool IsExpired()
+    {
+        return currentime >= targettime;
+    }
+
+    public bool IsPaused
+    {
+        get { return paused; }
+        set { paused = value; }
+    }
+    public float MaxTime
+    {
+        get { return targettime; }
+        set { targettime = value; }
+    }
+}
