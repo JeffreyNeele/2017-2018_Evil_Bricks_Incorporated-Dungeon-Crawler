c1a0b6f33d8d47442da74f790e8f176ac2703f79
Tim Schagen <t.schagen@students.uu.nl>
2017-11-29 09:36:20 +0100

    Particle effect classes toegevoegd en nog wat gefixt ten opzichte van CameraHelper.cs

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/CameraHelper.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/CameraHelper.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 
+// Class that mantains an internal offset for the actual camera for easy camera adjustment
 public class CameraHelper
 {
-    Vector2 windowSize, position;
+    Vector2 windowSize, position, cameraOffset;
     public CameraHelper(Vector2 windowSize)
     {
         this.position = Vector2.Zero;
+        cameraOffset = Vector2.Zero;
     }
     public void Reset(Vector2 playerPosition, Vector2 gameSize, Point screenSize)
     {
         position = Vector2.Zero;
     }
+    // Place the position of the Camera on the middle of the screen
     public void Center()
     {
         this.position = new Vector2(windowSize.X / 2, windowSize.Y / 2);
--- FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/TextGameObject.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/TextGameObject.cs
@@ -6,19 +6,31 @@ public class TextGameObject : GameObject
     protected SpriteFont spriteFont;
     protected Color color;
     protected string text;
-
+    protected Vector2 cameraOffset;
+    protected bool affectedByCamera;
+    CameraHelper cameraHelper;
     public TextGameObject(string assetname, int layer = 0, string id = "")
         : base(layer, id)
     {
         spriteFont = GameEnvironment.AssetManager.Content.Load<SpriteFont>(assetname);
         color = Color.White;
+        cameraOffset = Vector2.Zero;
+        affectedByCamera = true;
     }
 
+    public override void Update(GameTime gameTime)
+    {
+        if (affectedByCamera)
+        {
+            cameraOffset = GameEnvironment.CameraHelper.CameraOffset;
+        }
+        base.Update(gameTime);
+    }
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         if (visible)
         {
-            spriteBatch.DrawString(spriteFont, text, GlobalPosition, color);
+            spriteBatch.DrawString(spriteFont, text, GlobalPosition + cameraOffset, color);
         }
     }
 
