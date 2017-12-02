71cd5fc94474bf508c866211b7acfc11789ffbd7
Tim Schagen <t.schagen@students.uu.nl>
2017-12-02 14:39:45 +0100

    tussendoorse commit zodat ik op mn thuis computer verder kan gaan

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Playable Characters/Character.cs
@@ -0,0 +1,23 @@
+﻿
+class Character
+{
+    protected Attributes attributes;
+    public Character()
+    {
+        attributes = new Attributes();
+    }
+    protected  struct Attributes
+    {
+        public int hp, armour, gold;
+    }
+
+    public void Method()
+    {
+
+    }
+    public Character.Attributes d
+    {
+        get { return attributes; }
+        set { attributes = value; }
+    }
+}
