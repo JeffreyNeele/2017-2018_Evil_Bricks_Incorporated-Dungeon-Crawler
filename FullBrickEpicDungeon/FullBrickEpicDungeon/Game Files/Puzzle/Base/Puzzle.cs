27bf1a6639ab9dc5c4328561a821d558d79b0996
Jeffrey <j.t.a.neele@students.uu.nl>
2017-12-04 12:36:10 +0100

    Heb klasse puzzle en Traps toegevoegd

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Puzzle/Base/Puzzle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Klasse houdt de conditie van de puzzel bij en kijkt of de puzzel is afgerond
+class Puzzle : GameObjectList
+{
+    bool puzzleCleared = false;
+
+    public Puzzle()
+    {
+
+    }
+
+    public bool puzzleStatus
+    {
+        get { return puzzleCleared; }
+    }
+}
+
