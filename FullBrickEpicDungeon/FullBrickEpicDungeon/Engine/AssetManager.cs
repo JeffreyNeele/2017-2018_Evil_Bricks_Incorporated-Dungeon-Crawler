ff096dc239a080abde5c3632d0cd5850b3008903
Amber Elferink <a.s.elferink@students.uu.nl>
2017-12-05 17:20:55 +0100

    Errors gefixt

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/AssetManager.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/AssetManager.cs
@@ -15,7 +15,7 @@ public class AssetManager
 
     public Texture2D GetSprite(string assetName)
     {
-        assetName = Path.GetFullPath(assetName);
+      
         if (assetName == "")
         {
             return null;
