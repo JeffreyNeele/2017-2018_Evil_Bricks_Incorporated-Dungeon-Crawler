39f3789b073bc9407e402dd056dc0a2ed1b20fec
Amber Elferink <a.s.elferink@students.uu.nl>
2017-12-05 13:22:31 +0100

    Conversation basis aangepast + Paden werkend
    Let bij toevoegen van nieuwe bestanden/sprites in content op dat je Copy if Newer aanklikt bij Properties. Daarnaast heb ik een TestState aangemaakt tot de andere states klaar zijn om mee te werken.

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Game states/TestState.cs
@@ -0,0 +1,14 @@
+﻿
+    class TestState : GameObjectList
+{
+
+    Conversation conversation;
+
+    public TestState()
+    {
+        conversation = new Conversation();
+        
+        conversation.LoadConversation("Content/Conversations/conv_test.txt");
+    }
+}
+
