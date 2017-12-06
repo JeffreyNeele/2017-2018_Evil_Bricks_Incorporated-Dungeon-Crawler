8c40b04357e47f3a0174dcc0c939df6bcb675e2c
Tim Schagen <t.schagen@students.uu.nl>
2017-12-06 14:15:14 +0100

    voor jullie even een bug fix van de test state herinner je dat je deze altijd kan gebruiken :) veel succes

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Game states/TestState.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Game states/TestState.cs
@@ -4,30 +4,14 @@ using Microsoft.Xna.Framework.Graphics;
 
 class TestState : GameObjectList
 {
-    Conversation conversation;
-    AnimationTester animationTester;
-    public TestState()
-    {
-        /*
-        conversation = new Conversation();
-        
-        conversation.LoadConversation("Content/Conversations/conv_test.txt",0,1);
-        Add(conversation);
-        conversation.ShowConversation();
-        */
-        animationTester = new AnimationTester();
-        animationTester.TestAnimation();
-    }
-
+  
     public override void Update(GameTime gameTime)
     {
-        animationTester.Update(gameTime);
         base.Update(gameTime);
     }
 
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        animationTester.Draw(gameTime, spriteBatch);
         base.Draw(gameTime, spriteBatch);
     }
 }
--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Level/LevelLoading.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Level/LevelLoading.cs
@@ -2,7 +2,5 @@
 using System.Collections.Generic;
 partial class Level : GameObjectList
 {
-    // Mijn advies voor het level team is hier om een gameobjectgrid vande tiles te maken, gebaseerd op de lengte en design
-    // van het te laden text file. Misschien moet je daarvoor nog wel een TileField.cs klasse maken, zie tick tick hiervoor
 
 }
