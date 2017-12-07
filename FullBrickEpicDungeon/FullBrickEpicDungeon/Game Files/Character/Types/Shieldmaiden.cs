25b6817bf4506cca8e9fbc1263e42253678b89d3
patrick <p.h.klaassen@students.uu.nl>
2017-12-07 13:20:55 +0100

    added the 4 different classes, however still using the lightbringer sprite.

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Character/Types/Shieldmaiden.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+class Shieldmaiden : Character
+{
+    public Shieldmaiden() : base(ClassType.Lightbringer, "Sprites/Lightbringer/lightbringer_default", "Lightbringer")
+    {
+        // Loads the idle animation
+        LoadAnimation("Sprites/Lightbringer/lightbringer_idle@3", "lightbringer_idle", true, 0.33F);
+        // sets this characters base attributes, might be set in level later but for now it is in this constructors example.
+        this.attributes.HP = 0;
+        this.attributes.Armour = 0;
+        this.attributes.Attack = 0;
+        this.attributes.Gold = 0;
+    }
+}
+
