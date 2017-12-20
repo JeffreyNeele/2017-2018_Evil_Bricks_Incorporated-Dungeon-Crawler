e49a19a8a3b207f425bfa9c2d28c88b1cae9d374
Tim Schagen <t.schagen@students.uu.nl>
2017-12-20 12:56:13 +0100

    added circle class

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Engine/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+class Circle
+{
+    private double radius;
+    private Vector2 origin;
+    public Circle(double radius, Vector2 origin)
+    {
+        this.radius = radius;
+    }
+
+    public double Diameter
+    {
+        get { return radius * 2; }
+    }
+
+    public double Area
+    {
+        get
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+
+    public double Circumference
+    {
+        get { return 2 * Math.PI * radius; }
+    }
+
+    public double Radius
+    {
+        get { return radius; }
+    }
+
+    public Vector2 Origin
+    {
+        get { return origin; }
+    }
+
+    public bool CollidesWithRectangle(Rectangle rectangle)
+    {
+        float closestX = MathHelper.Clamp(origin.X, rectangle.Left, rectangle.Right);
+        float closestY = MathHelper.Clamp(origin.Y, rectangle.Top, rectangle.Bottom);
+        Vector2 distance = new Vector2(closestX, closestY);
+        double distanceSquared = (float)Math.Pow(distance.X, 2) + Math.Pow(distance.Y, 2);
+        return distanceSquared < Math.Pow(radius, 2);
+    }
+}
--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Monsters/Base/MonsterAI.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Monsters/Base/MonsterAI.cs
@@ -5,5 +5,40 @@ using Microsoft.Xna.Framework;
 // Basic AI for monsters will be defined here
 abstract partial class Monster : SpriteGameObject
 {
+    Character targetCharacter;
 
+    public void LineOfSightChecker(float sightRange)
+    {
+        GameObjectList playerList = GameWorld.Find("playerLIST") as GameObjectList;
+        Circle lineOfSight = new Circle(sightRange + sprite.Width / 2, this.origin);
+    }
+    public void TargetRandomPlayer(float chance)
+    {
+        GameObjectList playerList = GameWorld.Find("playerLIST") as GameObjectList;
+        foreach (Character player in playerList.Children)
+        {
+            int selectedNumber = GameEnvironment.Random.Next(0, 101);
+            if (selectedNumber <= chance)
+            {
+                this.targetCharacter = player;
+                break;
+            }
+        }
+
+        if(targetCharacter == null)
+        {
+            TargetRandomPlayer(chance);
+        }
+    }
+
+    public void AttackPlayer()
+    {
+        
+    }
+
+    public Character TargetCharacter
+    {
+        get { return targetCharacter; }
+        set { targetCharacter = value; }
+    }
 }
