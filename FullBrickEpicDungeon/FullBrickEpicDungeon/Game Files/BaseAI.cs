2b24e95d11837c252987feed0613899341fc08a0
Tim "Lightning2x" Schagen <schagen34@gmail.com>
2017-12-27 12:06:38 +0100

    quick fix

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/BaseAI.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/BaseAI.cs
@@ -58,6 +58,46 @@ class BaseAI : GameObject
         return new Vector2(adjacent, opposite);
     }
 
+    /*
+
+    Character targetCharacter;
+
+    public void LineOfSightChecker(float sightRange)
+    {
+        GameObjectList playerList = GameWorld.Find("playerLIST") as GameObjectList;
+        Circle lineOfSight = new Circle(sightRange + sprite.Width / 2, this.origin);
+    }
+    public void TargetRandomPlayer(float chance)
+    {
+        GameObjectList playerList = GameWorld.Find("playerLIST") as GameObjectList;
+        foreach (Character player in playerList.Children)
+        {
+            int selectedNumber = GameEnvironment.Random.Next(0, 101);
+            if (selectedNumber <= chance)
+            {
+                this.targetCharacter = player;
+                break;
+            }
+        }
+
+        if (targetCharacter == null)
+        {
+            TargetRandomPlayer(chance);
+        }
+    }
+
+    public void MoveToPlayer(Vector2 playerPosition)
+    {
+        Point[] path = pathFinder.FindPath(this.position, playerPosition);
+
+    }
+
+    public Character TargetCharacter
+    {
+        get { return targetCharacter; }
+        set { targetCharacter = value; }
+    }
+    */
     public Vector2 ParentMovementSpeed
     {
         get { return parentMovementSpeed; }
