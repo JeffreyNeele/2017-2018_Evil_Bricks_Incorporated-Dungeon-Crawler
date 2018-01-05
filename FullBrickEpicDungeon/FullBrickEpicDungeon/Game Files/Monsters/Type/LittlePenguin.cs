8e6749f5c662c78d1ea46ebd5663c30d683aa373
Jeffrey Neele <j.t.a.neele@students.uu.nl>
2018-01-05 14:23:19 +0100

    2 klassen toegevoegd. Zullen beide worden uitgebreid later

---
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Monsters/Type/LittlePenguin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+//class LittlePenguin : Monster
+//{
+//    public LittlePenguin() : base()
+//    {
+
+//    }
+//}
+
