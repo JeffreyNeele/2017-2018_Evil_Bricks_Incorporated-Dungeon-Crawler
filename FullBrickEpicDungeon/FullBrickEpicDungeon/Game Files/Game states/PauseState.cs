d97c44ff27b374ed80af9206f06916088654b613
Tim Schagen <t.schagen@students.uu.nl>
2018-01-12 14:43:02 +0100

    pause state nu met nieuwe sprites

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Game states/PauseState.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Game states/PauseState.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-//State dat een in-game menu weergeeft. In deze state wordt het spel gepauzeerd en kan er een aantal acties worden ondernomen.
+//State that pauses the game
 class PauseState : IGameLoopObject
 {
     protected IGameLoopObject playingState;
@@ -14,11 +14,13 @@ class PauseState : IGameLoopObject
 
     public PauseState()
     {
+        // find the playing state
         playingState = GameEnvironment.GameStateManager.GetGameState("playingState");
 
-        continueButton = new Button("Assets/Sprites/Menu/StartButton", 99);
+        // make buttons for the different assignments, eg return to menu
+        continueButton = new Button("Assets/Sprites/Paused/Continue", 99);
         continueButton.Position = new Vector2(GameEnvironment.Screen.X / 2 - continueButton.Width / 2, 250);
-        quitButton = new Button("Assets/Sprites/Menu/StartButton", 99);
+        quitButton = new Button("Assets/Sprites/Paused/ReturnToMenu", 99);
         quitButton.Position = new Vector2(GameEnvironment.Screen.X / 2 - quitButton.Width / 2, 500);
     }
 
@@ -31,10 +33,12 @@ class PauseState : IGameLoopObject
     {
         // we draw the playingstate but we do not update it because we want the pause state to be an overlay.
         playingState.Draw(gameTime, spriteBatch);
+        // draw the buttons
         continueButton.Draw(gameTime, spriteBatch);
         quitButton.Draw(gameTime, spriteBatch);
     }
 
+    // handles input
     public void HandleInput(InputHelper inputHelper)
     {
         continueButton.HandleInput(inputHelper);
@@ -43,6 +47,7 @@ class PauseState : IGameLoopObject
         {
             GameEnvironment.GameStateManager.SwitchTo("playingState");
         }
+        // if the quit button is pressed, we reset the playing state and return to the menu
         else if (quitButton.Pressed)
         {
             playingState.Reset();
