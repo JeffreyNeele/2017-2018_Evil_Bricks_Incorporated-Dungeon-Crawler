919165f41869f1ac975f6ae31100e63aeed940ed
Jeffrey Neele <j.t.a.neele@students.uu.nl>
2018-01-21 20:56:04 +0100

    tussen stap trapdoor functioneel

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Objects/Openable objects/Types/Trapdoor.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Objects/Openable objects/Types/Trapdoor.cs
@@ -9,11 +9,34 @@ class Trapdoor : OpenableObject
     /// <param name="assetname">Path to be able to load in the sprite</param>
     /// <param name="id">defined id to be able to find the door</param>
     /// <param name="sheetIndex">Defines which picture of the animation will be shown</param>
-    public Trapdoor(string assetname, string id, int sheetIndex) : base(TileType.Trapdoor, assetname, id, sheetIndex)
+    public Trapdoor(TileType door, string assetname, string id, int sheetIndex, Level level) : base(door, assetname, id, sheetIndex)
     {
+
     }
     public override void Update(GameTime gameTime)
     {
+        if (DetectAllPlayers() && this.Sprite.SheetIndex == 1)
+            GameEnvironment.GameStateManager.SwitchTo("levelFinishedState");
         base.Update(gameTime);
+    }
+
+    public bool DetectAllPlayers()
+    {
+        allPlayers = GameWorld.Find("playerLIST") as GameObjectList;
+        int onTrapdoorCounter = 0;
+        foreach (Character player in allPlayers.Children)
+        {
+            Rectangle quarterBoundingBox = new Rectangle((int)player.BoundingBox.X, (int)(player.BoundingBox.Y + 0.75 * Height), player.Width, (int)(player.Height / 4));
+            if (this.BoundingBox.Intersects(quarterBoundingBox))
+                onTrapdoorCounter++;
+        }
+
+        if (onTrapdoorCounter == 4)
+        {
+            return true;
+        }
+
+        return false;
+
     }
 }
