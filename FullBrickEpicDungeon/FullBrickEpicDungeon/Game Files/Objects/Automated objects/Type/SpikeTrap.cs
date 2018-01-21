501ccffb3001b8e569e7b2cd0c2b009ba0c84f73
Jeffrey Neele <j.t.a.neele@students.uu.nl>
2018-01-21 21:38:10 +0100

    mooiere indeling, spiketrap met inhoud

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Objects/Automated objects/Type/SpikeTrap.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Objects/Automated objects/Type/SpikeTrap.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FullBrickEpicDungeon.Game_Files.Objects.Automated_objects.Type
+class SpikeTrap : AutomatedObject
 {
-    class SpikeTrap
+    public SpikeTrap(string assetname, string id, int sheetIndex, Level level) : base(assetname, id, sheetIndex, level)
     {
+        duration = new Timer(2.0f);
+        duration.IsPaused = false;
+        setupTimer = new Timer(0.2f);
+        setupTimer.IsPaused = false;
     }
 }
