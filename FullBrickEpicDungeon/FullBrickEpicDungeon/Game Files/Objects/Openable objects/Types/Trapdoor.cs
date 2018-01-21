902b1a621f2f45f77571f9e9e8c01065728177a3
Jeffrey Neele <j.t.a.neele@students.uu.nl>
2018-01-21 20:57:20 +0100

    trapdoor functioneel

--- FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Objects/Openable objects/Types/Trapdoor.cs
+++ FullBrickEpicDungeon/FullBrickEpicDungeon/Game Files/Objects/Openable objects/Types/Trapdoor.cs
@@ -5,7 +5,7 @@
 /// </summary>
 class Trapdoor : OpenableObject
 {
-
+    GameObjectList allPlayers;
     /// <param name="assetname">Path to be able to load in the sprite</param>
     /// <param name="id">defined id to be able to find the door</param>
     /// <param name="sheetIndex">Defines which picture of the animation will be shown</param>
